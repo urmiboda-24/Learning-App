ee1d49c466edb5d0a36227a9e261da2cb9d51c06
Urmi Boda <urmi.boda@tatvasoft.com>
2026-01-30 15:08:01 +0530

    try to push directly in dev branch

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import "./App.css";
 
 function App() {
   const apiUrl = import.meta.env.VITE_API_URL;
+  var x = 10;
   return (
     <>
       <h1>Learning App</h1>
       <h4>{apiUrl}</h4>
+      <h3>{x}</h3>
     </>
   );
 }
